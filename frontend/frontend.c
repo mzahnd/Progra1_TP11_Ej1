271298c7dc0f70ad0d52f5c1f5828f017392615c
mzahnd <mzahnd@itba.edu.ar>
2019-10-31 21:21:41 -0300

    Instructions text added. Missing keyboard and audio functions.

--- frontend/frontend.c
+++ frontend/frontend.c
@@ -50,54 +50,89 @@
 #include "frontend.h"
 
 // ====== Constants and Macros ======
-#define D_WIDTH     640
-#define D_HEIGHT    480
+#define LEDS        8
+#define TXT_INST    6
 
-#define COLOR_MAIN_BKGND    "#FFFFFF"
-#define COLOR_LED_BORDER    "#000000"
-#define COLOR_LED_OFF       "#0A490A"
-#define COLOR_LED_ON        "#00FF00"
-#define COLOR_TXT_INST      "#000000"
+#define D_WIDTH     ( (2 * LEDS * LED_SIZE) + (2 * LEDS * LED_BORDER ) + \
+                    LED_SHIFT + LED_SPC_BTWN )
+#define D_HEIGHT    ( (2 * LED_SIZE) + (2 * LED_BORDER) + (2 * LED_SHIFT) + \
+                    ((TXT_INST_POS_Y) + (TXT_INST * TXT_INST_SIZE)) )
+
+#define MAIN_BKGND_COLOR    "#000000"
+#define LED_COLOR_BORDER    "#626567"
+#define LED_COLOR_OFF       "#0A490A"
+#define LED_COLOR_ON        "#00FF00"
+#define TXT_INST_COLOR      "#D7DBDD"
 
 // LEDs position and size in Main Window. Must be float
 // Radius
-#define SIZE_LED            20.0
-#define BORDER_THIKNESS_LED (SIZE_LED / 5.0)
+#define LED_SIZE            20.0
+#define LED_BORDER          (LED_SIZE / 5.0)
+
 // 0.5 added to ensure LED looks nice
-#define SIZE_INT_LED        SIZE_LED - (BORDER_THIKNESS_LED / 2) + 0.5
+#define LED_SIZE_INT        (LED_SIZE - (LED_BORDER / 2.0) + 0.5)
 
-#define SHIFT_LED_BORDER    10.0
-#define SPACE_BTWN_LEDS     2*SIZE_LED + BORDER_THIKNESS_LED + SHIFT_LED_BORDER
+// First LED Shift respect to top left corner (in both, x and y axis)
+#define LED_SHIFT           10.0
+// Space between LEDs
+#define LED_SPC_BTWN        ((2 * LED_SIZE) + (LED_BORDER) + LED_SHIFT)
 
-#define LED0_POS_X          SHIFT_LED_BORDER + SIZE_LED + BORDER_THIKNESS_LED
-#define LED0_POS_Y          SHIFT_LED_BORDER + SIZE_LED + BORDER_THIKNESS_LED
+#define LED_POS_X          (LED_SHIFT + LED_SIZE + LED_BORDER)
+#define LED_POS_Y          (LED_SHIFT + LED_SIZE + LED_BORDER)
 
-
-#define LED1_POS_X          LED0_POS_X + SPACE_BTWN_LEDS
-#define LED1_POS_Y          LED0_POS_Y
-
-#define LED2_POS_X          LED1_POS_X + SPACE_BTWN_LEDS
-#define LED2_POS_Y          LED0_POS_Y
-
-#define LED3_POS_X          LED2_POS_X + SPACE_BTWN_LEDS
-#define LED3_POS_Y          LED0_POS_Y
-
-#define LED4_POS_X          LED3_POS_X + SPACE_BTWN_LEDS
-#define LED4_POS_Y          LED0_POS_Y
-
-#define LED5_POS_X          LED4_POS_X + SPACE_BTWN_LEDS
-#define LED5_POS_Y          LED0_POS_Y
-
-#define LED6_POS_X          LED5_POS_X + SPACE_BTWN_LEDS
-#define LED6_POS_Y          LED0_POS_Y
-
-#define LED7_POS_X          LED6_POS_X + SPACE_BTWN_LEDS
-#define LED7_POS_Y          LED0_POS_Y
+/*
+ * @brief Look Up Table for LEDs position and colors
+ * 
+ * leds_lku[i][0] = LED i position in X axis
+ * 
+ * leds_lku[i][1] = LED i position in Y axis
+ * 
+ * leds_lku[i][2] = LED border size
+ * 
+ * leds_lku[i][3] = LED size
+ * 
+ * leds_lku[i][4] = LED border thikness
+ */
+static const float leds_lku[LEDS][5] = {
+    {LED_POS_X + (0 * LED_SPC_BTWN), LED_POS_Y,
+     LED_SIZE, LED_SIZE_INT, LED_BORDER},
+    {LED_POS_X + (1 * LED_SPC_BTWN), LED_POS_Y,
+     LED_SIZE, LED_SIZE_INT, LED_BORDER},
+    {LED_POS_X + (2 * LED_SPC_BTWN), LED_POS_Y,
+     LED_SIZE, LED_SIZE_INT, LED_BORDER},
+    {LED_POS_X + (3 * LED_SPC_BTWN), LED_POS_Y,
+     LED_SIZE, LED_SIZE_INT, LED_BORDER},
+    {LED_POS_X + (4 * LED_SPC_BTWN), LED_POS_Y,
+     LED_SIZE, LED_SIZE_INT, LED_BORDER},
+    {LED_POS_X + (5 * LED_SPC_BTWN), LED_POS_Y,
+     LED_SIZE, LED_SIZE_INT, LED_BORDER},
+    {LED_POS_X + (6 * LED_SPC_BTWN), LED_POS_Y,
+     LED_SIZE, LED_SIZE_INT, LED_BORDER},
+    {LED_POS_X + (7 * LED_SPC_BTWN), LED_POS_Y,
+     LED_SIZE, LED_SIZE_INT, LED_BORDER},
+};
 
 // Instructions
 // Must be int
-#define SIZE_INSTRUCTIONS       36
-#define PATH_FONT_INSTRUCT  "/fonts/liberation_serif/LiberationSerif-Regular.ttf"
+#define TXT_INST_SIZE   18
+#define TXT_INST_PATH_FONT \
+                    "frontend/fonts/liberation_serif/LiberationSerif-Bold.ttf"
+
+#define TXT_INST_POS_X      (LED_POS_X - LED_SIZE - LED_BORDER)
+// Separation from LEDs + LED_POS_Y + LED Diameter + LED_BORDER (twice)
+#define TXT_INST_POS_Y      (TXT_INST_SPC_BTWN + LED_SHIFT + (2 * LED_SIZE) + \
+                            (2 * LED_BORDER))
+
+#define TXT_INST_SPC_BTWN   (10.0 + TXT_INST_SIZE)
+
+static const float txt_inst_lku[TXT_INST][2] = {
+    {TXT_INST_POS_X, TXT_INST_POS_Y + 0 * TXT_INST_SPC_BTWN},
+    {TXT_INST_POS_X, TXT_INST_POS_Y + 1 * TXT_INST_SPC_BTWN},
+    {TXT_INST_POS_X, TXT_INST_POS_Y + 2 * TXT_INST_SPC_BTWN},
+    {TXT_INST_POS_X, TXT_INST_POS_Y + 3 * TXT_INST_SPC_BTWN},
+    {TXT_INST_POS_X, TXT_INST_POS_Y + 4 * TXT_INST_SPC_BTWN},
+    {TXT_INST_POS_X, TXT_INST_POS_Y + 5 * TXT_INST_SPC_BTWN},
+};
 
 // ====== Prototypes ======
 // Draw LEDs in Main Window
@@ -156,8 +191,8 @@ int
 main_window()
 {
     ALLEGRO_DISPLAY *display = NULL;
-    ALLEGRO_EVENT_QUEUE *evq = NULL;
-    ALLEGRO_TIMER *timer = NULL;
+    //ALLEGRO_EVENT_QUEUE *evq = NULL;
+    //ALLEGRO_TIMER *timer = NULL;
 
     // Start display
     display = al_create_display(D_WIDTH, D_HEIGHT);
@@ -173,10 +208,13 @@ main_window()
     // Draw display
 
     // Background
-    al_clear_to_color(al_color_html(COLOR_MAIN_BKGND));
+    al_clear_to_color(al_color_html(MAIN_BKGND_COLOR));
 
     // LEDs
     draw_leds();
+
+    // Instructions
+    printInstructions();
 
     // Show
     al_flip_display();
@@ -198,54 +236,41 @@ draw_leds()
      * 2.- Inside of the LED
      */
 
-    // LED0
-    al_draw_circle(LED0_POS_X, LED0_POS_Y, SIZE_LED,
-                   al_color_html(COLOR_LED_BORDER), BORDER_THIKNESS_LED);
-    al_draw_filled_circle(LED0_POS_X, LED0_POS_Y, SIZE_INT_LED,
-                          al_color_html(COLOR_LED_OFF));
 
-    // LED1
-    al_draw_circle(LED1_POS_X, LED1_POS_Y, SIZE_LED,
-                   al_color_html(COLOR_LED_BORDER), BORDER_THIKNESS_LED);
-    al_draw_filled_circle(LED1_POS_X, LED1_POS_Y, SIZE_INT_LED,
-                          al_color_html(COLOR_LED_OFF));
+    /*
+     * @brief Look Up Table for LEDs position and colors
+     * 
+     * leds_lku[i][0] = LED i position in X axis
+     * 
+     * leds_lku[i][1] = LED i position in Y axis
+     * 
+     * leds_lku[i][2] = LED border size
+     * 
+     * leds_lku[i][3] = LED size
+     * 
+     * leds_lku[i][4] = LED border thikness
+     * 
+     * leds_lku[i][5] = LED i border color
+     * 
+     * leds_lku[i][6] = LED i OFF color
+     * 
+     * leds_lku[i][7] = LED i ON color
+     */
 
-    // LED2
-    al_draw_circle(LED2_POS_X, LED2_POS_Y, SIZE_LED,
-                   al_color_html(COLOR_LED_BORDER), BORDER_THIKNESS_LED);
-    al_draw_filled_circle(LED2_POS_X, LED2_POS_Y, SIZE_INT_LED,
-                          al_color_html(COLOR_LED_OFF));
+    uint16_t i;
+    for(i = 0; i < LEDS; i++)
+    {
+        // Border
 
-    // LED3
-    al_draw_circle(LED3_POS_X, LED3_POS_Y, SIZE_LED,
-                   al_color_html(COLOR_LED_BORDER), BORDER_THIKNESS_LED);
-    al_draw_filled_circle(LED3_POS_X, LED3_POS_Y, SIZE_INT_LED,
-                          al_color_html(COLOR_LED_OFF));
+        al_draw_circle(leds_lku[i][0], leds_lku[i][1], leds_lku[i][2],
+                       al_color_html(LED_COLOR_BORDER), leds_lku[i][4]);
 
-    // LED4
-    al_draw_circle(LED4_POS_X, LED4_POS_Y, SIZE_LED,
-                   al_color_html(COLOR_LED_BORDER), BORDER_THIKNESS_LED);
-    al_draw_filled_circle(LED4_POS_X, LED4_POS_Y, SIZE_INT_LED,
-                          al_color_html(COLOR_LED_OFF));
+        // LED
 
-    // LED5
-    al_draw_circle(LED5_POS_X, LED5_POS_Y, SIZE_LED,
-                   al_color_html(COLOR_LED_BORDER), BORDER_THIKNESS_LED);
-    al_draw_filled_circle(LED5_POS_X, LED5_POS_Y, SIZE_INT_LED,
-                          al_color_html(COLOR_LED_OFF));
+        al_draw_filled_circle(leds_lku[i][0], leds_lku[i][1], leds_lku[i][3],
+                              al_color_html(LED_COLOR_OFF));
 
-    // LED6
-    al_draw_circle(LED6_POS_X, LED6_POS_Y, SIZE_LED,
-                   al_color_html(COLOR_LED_BORDER), BORDER_THIKNESS_LED);
-    al_draw_filled_circle(LED6_POS_X, LED6_POS_Y, SIZE_INT_LED,
-                          al_color_html(COLOR_LED_OFF));
-
-    // LED7
-    al_draw_circle(LED7_POS_X, LED7_POS_Y, SIZE_LED,
-                   al_color_html(COLOR_LED_BORDER), BORDER_THIKNESS_LED);
-    al_draw_filled_circle(LED7_POS_X, LED7_POS_Y, SIZE_INT_LED,
-                          al_color_html(COLOR_LED_OFF));
-
+    }
 }
 
 // Print instructions
@@ -254,7 +279,7 @@ static int
 printInstructions(void)
 {
     ALLEGRO_FONT *instruct_font = NULL;
-    instruct_font = al_load_font(PATH_FONT_INSTRUCT, SIZE_INSTRUCTIONS, 0);
+    instruct_font = al_load_font(TXT_INST_PATH_FONT, TXT_INST_SIZE, 0);
 
     if(!instruct_font)
     {
@@ -262,16 +287,29 @@ printInstructions(void)
         return AL_ERR_CODE;
     }
 
-    //al_draw_text(intruct_font, COLOR_TXT_INST);
+    al_draw_text(instruct_font, al_color_html(TXT_INST_COLOR),
+                 txt_inst_lku[0][0], txt_inst_lku[0][1], 0,
+                 "Press a LED number (0 - 7) to turn it on.");
 
-    /*
-    puts(TEXT_COLOR "\nPress a LED number (0 - 7) to turn it on.");
-    puts(TEXT_COLOR "Press \'t\' to toggle all LEDs status.");
-    puts(TEXT_COLOR "Press \'s\' to turn ON all LEDs.");
-    puts(TEXT_COLOR "Press \'c\' to turn OFF all LEDs.");
-    puts(TEXT_COLOR "Press \'q\' to exit.");
+    al_draw_text(instruct_font, al_color_html(TXT_INST_COLOR),
+                 txt_inst_lku[1][0], txt_inst_lku[1][1], 0,
+                 "Press \'b\' to blink the LEDs.");
 
-     */
+    al_draw_text(instruct_font, al_color_html(TXT_INST_COLOR),
+                 txt_inst_lku[2][0], txt_inst_lku[2][1], 0,
+                 "Press \'c\' to turn OFF all LEDs.");
+
+    al_draw_text(instruct_font, al_color_html(TXT_INST_COLOR),
+                 txt_inst_lku[3][0], txt_inst_lku[3][1], 0,
+                 "Press \'s\' to turn ON all LEDs.");
+
+    al_draw_text(instruct_font, al_color_html(TXT_INST_COLOR),
+                 txt_inst_lku[4][0], txt_inst_lku[4][1], 0,
+                 "Press \'t\' to toggle all LEDs status.");
+
+    al_draw_text(instruct_font, al_color_html(TXT_INST_COLOR),
+                 txt_inst_lku[5][0], txt_inst_lku[5][1], 0,
+                 "Press \'q\' to exit.");
 
     return AL_OK_CODE;
 }
